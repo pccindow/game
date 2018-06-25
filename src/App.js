bb24515baaa1422dffcb381588ddb820841dfc13
callac <pccindo78@msn.com>
2018-06-25 16:39:24 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+//import ReactDOM from 'react-dom';
+//import React, { Component } from 'react';
+import './App.css';
+import Countdown from './countdown.js';
+import FindWords from './findwords.js';
+
+class App extends React.Component {
+  constructor(props) {
+  super(props);
+  this.state = { title: 'Entre le mot le plus rapidement possible' }
+  }
+  render() {
+    return (
+      <div id="box-content">
+        <h1> {this.state.title} </h1>
+        <Countdown />
+        <FindWords />
+      </div>
+    );
+  }
+}
+
+export default App;
